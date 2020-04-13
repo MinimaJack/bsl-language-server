e72a3d3d968604abce3526bb8e55365a56c3e3f2
github-action-benchmark <github@users.noreply.github.com>
2020-04-13 10:34:05 +0000

    add BSL LS perfomance measurement (SSL 3.1) (pytest) benchmark result for 4ba04dfa567cc70a030ccba456d44fc967aa2b89

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1586774045529,
+  "repoUrl": "https://github.com/MinimaJack/bsl-language-server",
+  "entries": {
+    "BSL LS perfomance measurement (SSL 3.1)": [
+      {
+        "commit": {
+          "author": {
+            "email": "nixel2007@gmail.com",
+            "name": "Nikita Gryzlov",
+            "username": "nixel2007"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ba04dfa567cc70a030ccba456d44fc967aa2b89",
+          "message": "Merge pull request #1039 from 1c-syntax/fix/docs\n\nИсправление в документации",
+          "timestamp": "2020-04-12T13:33:05+03:00",
+          "tree_id": "13d755e4b99fc6e61ee6d690f3f5426e4b56543c",
+          "url": "https://github.com/MinimaJack/bsl-language-server/commit/4ba04dfa567cc70a030ccba456d44fc967aa2b89"
+        },
+        "date": 1586774044892,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": ".github/scripts/benchmark.py::test_analyze_ssl31",
+            "value": 79.5458430449168,
+            "unit": "sec",
+            "range": "stddev: 1.0138541082600396",
+            "extra": "mean: 79.5458430449168 sec\nrounds: 3"
+          }
+        ]
+      }
+    ]
+  }
+}
